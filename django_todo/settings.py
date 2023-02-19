5c4417501b20947cef51b52c17183cad97595ed4
fenton1000 <fenton1000@yahoo.co.uk>
2023-02-19 23:42:44 +0000

    Add allowed host

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'THE_SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['full-stack-hello-django.herokuapp.com']
 
 
 # Application definition
